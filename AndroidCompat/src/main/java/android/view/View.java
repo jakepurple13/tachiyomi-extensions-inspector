d3a2120b60194c50bcb755770a2353fefcba3f63
Jacob Rein <jakerein@gmail.com>
2023-07-21 06:57:32 -0400

    Update

--- AndroidCompat/src/main/java/android/view/View.java
+++ AndroidCompat/src/main/java/android/view/View.java
@@ -18,4 +18,18 @@ public class View implements Drawable.Callback {
     public void unscheduleDrawable(Drawable who, Runnable what) {
 
     }
+
+    public interface OnCreateContextMenuListener {
+        /**
+         * Called when the context menu for this view is being built. It is not
+         * safe to hold onto the menu after this method returns.
+         *
+         * @param menu The context menu that is being built
+         * @param v The view for which the context menu is being built
+         * @param menuInfo Extra information about the item for which the
+         *            context menu should be shown. This information will vary
+         *            depending on the class of v.
+         */
+        void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo);
+    }
 }
